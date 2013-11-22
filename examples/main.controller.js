1529b8573065cad941233b80dd9cee45a41224c1
ChinmayMK <chinmaymkulkarni@gmail.com>
2013-11-22 09:05:50 +0530

    adding basic pie chart

--- examples/main.controller.js
+++ examples/main.controller.js
@@ -1,4 +1,11 @@
 
-function MainController($scope) {
+angular.module('example', ['angularCharts']);
+
+function MainController($scope, $interval) {
 	$scope.greeting = "Hello World";
+	$scope.data = [123,43,234,140,456,170];
+
+	$interval(function(){
+		$scope.data.push(Math.random() * 1000);
+	},500)
 }
