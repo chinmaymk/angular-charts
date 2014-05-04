5bc7770a8aa3262d149f3db7594e14dfd3576425
Felipe Amorim <felipe.leusin@gmail.com>
2014-05-04 18:05:22 -0300

    removed /dist from source control

--- dist/templates.js
+++
@@ -1,9 +0,0 @@
-angular.module('angularChartsTemplates', ['left']);
-
-angular.module("left", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("left",
-    "<div class='ac-legend' style='float:left; witdh:25%'>\n" +
-    "</div>\n" +
-    "<div class='ac-chart' style='float:left; witdh:75%'>\n" +
-    "</div>");
-}]);
