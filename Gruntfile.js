2dd5a565ae875c2a57c14e4bb28f132d26e7658d
Felipe Amorim <felipe.leusin@gmail.com>
2014-05-12 11:24:29 -0300

    fixed version template

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,8 +65,8 @@ module.exports = function(grunt) {
           'git checkout master -- bower.json',
           'mv -f dist/angular-charts.tmp.js dist/angular-charts.js',
           'mv -f dist/angular-charts.min.tmp.js dist/angular-charts.min.js',
-          "git commit -am 'release <%= pkg.version =%>'",
-          'git tag <%= pkg.version =%>'
+          "git commit -am 'release <%= pkg.version %>'",
+          'git tag <%= pkg.version %>'
         ].join('&&')
       }
     }
