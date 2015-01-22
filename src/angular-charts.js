487205efd2584c5390be897da8aa9260ae7a667d
Guillaume Pierronnet <guillaume@dataaddict.fr>
2015-01-22 19:11:45 +0100

    fixed infinite loop when jQuery is loaded

--- src/angular-charts.js
+++ src/angular-charts.js
@@ -58,7 +58,7 @@ angular.module('angularCharts').directive('acChart', function($templateCache, $c
   function getChildrenByClassname(childrens, className) {
     var child = null;
 
-    for (var i in childrens) {
+    for (var i = 0; i < childrens.length; i++) {
       if (angular.isElement(childrens[i])) {
         child = angular.element(childrens[i]);
         if (child.hasClass(className))
